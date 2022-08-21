e5a7aba9cdf907d0c4d820d051be8af5128070b2
Mihai Razvan <MIHAIRAZVAN03@YAHOO.COM>
2022-08-21 20:24:42 +0300

    water

--- Project-W/Assets/Scripts/Player.cs
+++ Project-W/Assets/Scripts/Player.cs
@@ -5,15 +5,13 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     [SerializeField]
-    private float speed;
+    float speed;
 
-    // Start is called before the first frame update
     void Start()
     {
         
     }
 
-    // Update is called once per frame
     void Update()
     {
         movement();
--- Project-W/Assets/Scripts/Water.cs
+++ Project-W/Assets/Scripts/Water.cs
@@ -2,17 +2,73 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(MeshFilter))]
 public class Water : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [SerializeField]
+    int xSize;
+    [SerializeField]
+    int zSize;
+    Mesh mesh;
+    Vector3[] vertices;
+    int[] triangles;
+    Vector2[] uvs;
+
     void Start()
     {
-        
+        mesh = new Mesh();
+        GetComponent<MeshFilter>().mesh = mesh;
+        CreateShape();
+        UpdateMesh();
+        Vector3 playerPos = FindObjectOfType<Player>().getPosition();
+        transform.position = new Vector3(playerPos.x - xSize / 2, 0, playerPos.z - zSize / 2);
     }
 
-    // Update is called once per frame
-    void Update()
+    void CreateShape()
     {
-        transform.position = FindObjectOfType<Player>().getPosition();
+        vertices = new Vector3[(xSize + 1) * (zSize + 1)];
+        uvs = new Vector2[(xSize + 1) * (zSize + 1)];
+
+        for (int i = 0, z = 0; z <= zSize; z++)
+        {
+            for (int x = 0; x <= xSize; x++)
+            {
+                vertices[i] = new Vector3(x, 0, z);
+                uvs[i] = new Vector2(((float)(x + (xSize / 2)) / xSize), ((float)(z + (zSize / 2)) / zSize));
+                i++;
+            }
+        }
+
+        triangles = new int[xSize * zSize * 6];
+
+        int vert = 0;
+        int tris = 0;
+
+        for (int z = 0; z < zSize; z++)
+        {
+            for (int x = 0; x < xSize; x++)
+            {
+                triangles[tris + 0] = vert + 0;
+                triangles[tris + 1] = vert + xSize + 1;
+                triangles[tris + 2] = vert + 1;
+                triangles[tris + 3] = vert + 1;
+                triangles[tris + 4] = vert + xSize + 1;
+                triangles[tris + 5] = vert + xSize + 2;
+
+                vert++;
+                tris += 6;
+            }
+            vert++;
+        }
+
+    }
+
+    void UpdateMesh()
+    {
+        mesh.Clear();
+        mesh.vertices = vertices;
+        mesh.triangles = triangles;
+        mesh.uv = uvs;
+        mesh.RecalculateNormals();
     }
 }
