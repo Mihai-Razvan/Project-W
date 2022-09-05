52a5deaae942964ae04e04b14eaa20281a58189d
Mihai Razvan <MIHAIRAZVAN03@YAHOO.COM>
2022-09-05 16:42:00 +0300

    almost finished ray

--- Project-W/Assets/Scripts/Items/Tools/Item_002.cs
+++ Project-W/Assets/Scripts/Items/Tools/Item_002.cs
@@ -8,6 +8,7 @@ public class Item_002 : Tool
 
     LineRenderer laserLine;
     float laserSize;   //the laser isn't full size since beginning, it is expanding during a few seconds; it starts from 0 and then increase, until reaches it final destination
+    [SerializeField]
     int maxLaserSize;
     [SerializeField]
     float laserExpansionSpeed;    //the speed at which the laser is expansing or retrating
@@ -15,12 +16,13 @@ public class Item_002 : Tool
     float rayRadius;               //the radius for the capsule collider that si cheking for the ray collision
     string laserState;
     Transform rayStartPosition;
+    [SerializeField]
+    LayerMask resourcesMask;
      
     void Start()
     {
         itemCode = 2;
         laserSize = 0;
-        maxLaserSize = 9;
         laserState = "UNUSED";
     }
 
@@ -51,7 +53,8 @@ public class Item_002 : Tool
         laserLine.positionCount = 2;         //so the ray appears; if we don't do this after setting it to 0 rat won't display
         laserLine.SetPosition(0, rayStartPosition.position);
       
-        laserLine.SetPosition(1, Camera.main.transform.position + Camera.main.transform.forward * laserSize);
+        laserLine.SetPosition(1, rayStartPosition.position + rayStartPosition.forward * laserSize);
+        Debug.Log(laserSize);
 
         switch(laserState)
         {
@@ -76,23 +79,37 @@ public class Item_002 : Tool
 
     void makeCollider()    //creates and manages the collider around the ray to check for resources
     {
-        Collider[] colliders = Physics.OverlapCapsule(rayStartPosition.position, Camera.main.transform.position + Camera.main.transform.forward * laserSize, rayRadius);
+        Collider[] colliders = Physics.OverlapCapsule(rayStartPosition.position, rayStartPosition.position + rayStartPosition.forward * laserSize, rayRadius, resourcesMask);
 
-        for (int i = 0; i < colliders.Length; i++)
-            Destroy(colliders[i].gameObject.GetComponent<Rigidbody>());
-
-        if(colliders.Length == 1)
-        {
-            laserState = "RETRACTING";
-            laserSize = Vector3.Distance(colliders[0].transform.position, rayStartPosition.position);
-            colliders[0].transform.position = Camera.main.transform.position + Camera.main.transform.forward * laserSize;
-            if (laserSize < 2)
+        if(colliders.Length != 0)
+            switch(laserState)
             {
-                FindObjectOfType<Inventory>().addItem(colliders[0].tag, 1);
-                Destroy(colliders[0].gameObject);
-            }
-        }
+                case "EXPANDING":  //if is expanding and we have a collision it means it is the only resources colliding since there are virtually 0 chances to have 2 resources colliding for the first time in the same frame
+                    laserState = "RETRACTING";
+                    laserSize = Vector3.Distance(colliders[0].transform.position, rayStartPosition.position);
+                    colliders[0].transform.position = rayStartPosition.position + rayStartPosition.forward * laserSize;
+                    Destroy(colliders[0].gameObject.GetComponent<Rigidbody>());
+                    break;
+                case "RETRACTING":
+                    for (int i = 0; i < colliders.Length; i++)
+                    {
+                        if (colliders[i].gameObject.GetComponent<Rigidbody>() != null)
+                        {
+                            Destroy(colliders[i].gameObject.GetComponent<Rigidbody>());
+                            colliders[i].transform.position = rayStartPosition.position + rayStartPosition.forward * Vector3.Distance(rayStartPosition.position, colliders[i].transform.position);
+                        }
 
+                       colliders[i].transform.position = colliders[i].transform.position - rayStartPosition.forward * laserExpansionSpeed * Time.deltaTime;   //it gets closer everty frame
+
+                        if(Vector3.Distance(colliders[i].transform.position, rayStartPosition.position) < 0.5f)
+                        {
+                            FindObjectOfType<Inventory>().addItem(colliders[i].tag, 1);
+                            Destroy(colliders[i].gameObject);
+                        }
+                    }
+
+                    break;
+            }
     }
 
 
