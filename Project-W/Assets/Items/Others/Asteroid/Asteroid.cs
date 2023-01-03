092c3fbe17da61b039faa75fbf24862276c27455
Mihai Razvan <MIHAIRAZVAN03@YAHOO.COM>
2023-01-03 22:04:15 +0200

    finished asteroid

--- Project-W/Assets/Items/Others/Asteroid/Asteroid.cs
+++ Project-W/Assets/Items/Others/Asteroid/Asteroid.cs
@@ -11,27 +11,58 @@ public class Asteroid : MonoBehaviour
     float collisionSphereRadius;
     [SerializeField]
     LayerMask placeableMask;
+    [SerializeField]
+    GameObject asteroidHitPrefab;
+    [SerializeField]
+    float playerDistanceDamage;        //the distance at which player has to be from the asteroid to get damaged
+    [SerializeField]
+    float playerHitDamage;           //the damage player gets when hit by the asteroid
+    [SerializeField]
+    float despawnLevel;
+    bool playerAlreadyHit;       //so the player doesn't get damage in consecutive frames; player can get damage only once by an asteroid
 
     void Start()
     {
         transform.LookAt(hitPoint);
+        playerAlreadyHit = false;
     }
 
     void Update()
     {
         transform.position = Vector3.MoveTowards(transform.position, hitPoint, speed * Time.deltaTime);
-        checkCollision();
+        checkPlaceablesCollision();
+        checkPlayerCollsion();
 
-        if (Vector3.Distance(transform.position, hitPoint) <= 0.3f)
+        if (Vector3.Distance(transform.position, hitPoint) <= 0.05f)
+            Destroy(this.gameObject);
+
+        if (transform.position.y <= despawnLevel)  //normally the asteroid should hit platforms and be destroyed, but if smth strange happens and it doesn't get destroyed we destroy it under the clouds
             Destroy(this.gameObject);
     }
 
-    void checkCollision()
+    void checkPlaceablesCollision()
     {
         Collider[] colliders = Physics.OverlapSphere(transform.position, collisionSphereRadius, placeableMask);
 
-        for(int i = 0; i < colliders.Length; i++)
+        for (int i = 0; i < colliders.Length; i++)
+        {
+            Instantiate(asteroidHitPrefab, colliders[i].gameObject.transform.position, Quaternion.identity);
             Destroy(colliders[i].gameObject);
+        }
+    }
+
+    void checkPlayerCollsion()
+    {
+        if (playerAlreadyHit)
+            return;
+
+        float distance = Vector3.Distance(this.gameObject.transform.position, FindObjectOfType<Player>().getPlayerPosition());
+
+        if (distance <= playerDistanceDamage)
+        {
+            FindObjectOfType<Player_Stats>().changeHealth(-playerHitDamage);
+            playerAlreadyHit = true;
+        }
     }
     
     public void setHitPoint(Vector3 point)
---
+++ Project-W/Assets/Items/Others/Asteroid/Asteroid_Hit.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Asteroid_Hit : MonoBehaviour
+{
+    float timeSinceSpawned;
+    void Start()
+    {
+        timeSinceSpawned = 0;
+    }
+
+
+    void Update()
+    {
+        timeSinceSpawned += Time.deltaTime;
+
+        if (timeSinceSpawned >= 15)
+            Destroy(this.gameObject);
+    }
+}
